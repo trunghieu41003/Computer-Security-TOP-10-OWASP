2908fa5a26be9eeff2a948d844b3cba18f25a101
DilysT <21522624@gm.uit.edu.vn>
2024-11-26 15:38:51 +0700

    trong database thêm cột login_attempts và locked_until vào users

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+
+
+
 const express = require('express')
 const userRoutes = require('./routes/auth.route'); // Import routes cho người dùng
 const authMiddleware = require('./midddleware/jwt');
@@ -5,12 +8,21 @@ const path = require('path');
 const app = express()
 const cors = require('cors'); // Import cors
 app.use(cors());
+
 // Middleware để phân tích cú pháp JSON
 app.use(express.json());
 
 // Thiết lập thư mục tĩnh cho các file HTML, CSS, JS
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Sử dụng routes cho người dùng
 app.use('/', userRoutes); // Tất cả các route liên quan đến người dùng sẽ bắt đầu với /users
 
-app.listen(3000)
+// Lắng nghe trên cổng 3000 và hiển thị thông báo xác nhận
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on http://0.0.0.0:3000');
+  });
+  
+
+
