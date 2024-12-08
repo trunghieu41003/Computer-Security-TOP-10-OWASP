c031f2665fe83ee2c341379bb52c6b8d1fb71887
trunghieu41003 <146319700+trunghieu41003@users.noreply.github.com>
2024-12-08 22:15:43 +0700

    update code A02

--- src/config/database.js
+++ src/config/database.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    port: 3308,
+    port: process.env.PORT_DB,
 });
 
 module.exports = pool;
